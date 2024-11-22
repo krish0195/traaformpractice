32112b413ac47df1ec2490ff6c471edfba740b6c
krishnaraj <krish0195@gmail.com>
2024-11-22 12:42:56 +0530

    terraform done

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"  # Change this to your preferred AWS region
 
 # Launch an EC2 instance
 resource "aws_instance" "example_instance" {
-  ami           = "ami-066784287e358dad1"  # Amazon Linux 2 AMI (change as needed)
+  ami           = "ami-0866a3c8686eaeeba"  # Amazon Linux 2 AMI (change as needed)
   instance_type = "t2.micro"               # Instance type (you can choose other types)
 
   tags = {
